9ec62db3c2f1c76a92bf952628acdab65547ea16
umer huzaifa <umerhuzaifa42@gmail.com>
2023-12-29 00:51:25 +0500

    Testing project structure

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const { env } = require('process');
-const debug = require("debug")("node-angular")
-const Port = process.env.Port || 3000;
-const app =  require('./Backend/app')
-app.set('port',Port);
-const server = http.createServer(app)
-
-server.listen(Port)
